bdd1d9e013048d992533f200ab0bee1943006475
HarshithRajesh <harshithr2004@gmail.com>
2023-06-05 00:18:39 +0530

    data from login and register page

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template, request,url_for
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("login.html")
 
+@app.route("/login",methods=["GET","POST"])
+def login():
+   if request.method=="POST":
+       username = request.form.get('user')
+       password = request.form.get('pass')
+
+       return username,password
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+        username = request.form.get("user")
+        password = request.form.get("pass")
+        email = request.form.get("email")
+
+    return username,password,email
+        
+
+       
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
