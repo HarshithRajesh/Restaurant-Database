fc13cd96b938d1d8c6f684b57d14cf878274fbdb
HarshithRajesh <harshithr2004@gmail.com>
2023-06-12 23:00:46 +0530

    checkbox page and id value is sorted for registration

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import psycopg2
 from werkzeug.security import generate_password_hash, check_password_hash
 
 conn = psycopg2.connect(dbname='Restaurent', user='postgres' ,password='babe123')
-
+COUNT = 3
 cur = conn.cursor()
 create_table = '''
     CREATE TABLE IF NOT EXISTS user_info (
@@ -45,26 +45,55 @@ def register():
         password = request.form.get("pass")
         repass = request.form.get("repass")
         email = request.form.get("email")
+        
 
         if (password==repass):
             password = generate_password_hash(password,method='pbkdf2:sha256',salt_length=8)
-
-            user_data = 'INSERT INTO user_info(name,password,email) VALUES (%s,%s,%s);'
-            values = (username,password,email)
-            
+            global COUNT
+            COUNT+=1
+            ex ='SELECT id FROM user_info ORDER BY id DESC LIMIT(1)'
+            cur.execute(ex)
+            last_id = cur.fetchall()
+            last_id = last_id[0][0]+1
+            print(last_id)
+            print(type(last_id))
+            user_data = 'INSERT INTO user_info(id,name,password,email) VALUES (%s,%s,%s,%s);'
+            values = (last_id,username,password,email)
+            print(last_id)
             cur.execute(user_data,values)
             conn.commit()
             cur.close()
             conn.close()
+            redire
+            return{'status':1}
         else:
             return {'status':0}
 
 
 
-    
-@app.route("/home",methods=["POST"])
+@app.route("/home",methods=["GET"])
 def home_page():
     return render_template("index.html")
+    # if request.method=="POST":
+
+    #     breakfast = request.form.get("breakfast")
+    #     lunch = request.form.ger("lunch")
+    #     snacks = request.form.get("snacks")
+    #     dinner = request.form.get("dinner")
+    #     ordered= []
+    #     if breakfast:
+    #         ordered.append("breakfast")
+    #     if lunch:
+    #         ordered.append("lunch")
+    #     if snacks:
+    #         ordered.append("snacks")
+    #     if dinner:
+    #         ordered.append("dinner")
+    #     else:
+    #         ordered.append("null")
+    
+    # return ordered
+        
        
 
 if __name__ == "__main__":
